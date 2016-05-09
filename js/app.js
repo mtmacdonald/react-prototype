8f79f55fb71dbb935e98041c76f54a3bf842d11f
Mark Macdonald <mark.t.macdonald@googlemail.com>
2016-05-09 14:12:09 +0200

    component nesting.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
-var CommentBox = React.createClass({
+var Parent = React.createClass({
   render: function() {
     return (
       <div className="component">
         Component.
+        <Child />
+      </div>
+    );
+  }
+});
+
+var Child = React.createClass({
+  render: function() {
+    return (
+      <div className="subcomponent">
+        Child component.
       </div>
     );
   }
 });
 
 ReactDOM.render(
-    <CommentBox />,
+    <Parent />,
     document.getElementById('app')
 );
