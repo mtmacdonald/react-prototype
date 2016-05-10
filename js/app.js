545bd681bd41bdf7e4d408ee1629d7e098ff304c
Mark Macdonald <mark.t.macdonald@googlemail.com>
2016-05-10 10:50:05 +0200

    added data passing.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,37 @@
-var Parent = React.createClass({
-  render: function() {
-    return (
-      <div className="component">
-        Component.
-        <Child />
-      </div>
-    );
-  }
+var data = [
+    {id: 1, title: "Item 1", text: "Details 1"},
+    {id: 2, title: "Item 2", text: "Details 2"}
+];
+
+var Page = React.createClass({
+    render: function() {
+        return (
+            <div className="container">
+                <h1>React Prototype</h1>
+                <List data={this.props.data}/>
+            </div>
+        );
+    }
 });
 
-var Child = React.createClass({
-  render: function() {
-    return (
-      <div className="subcomponent">
-        Child component.
-      </div>
-    );
-  }
+var List = React.createClass({
+    render: function() {
+        var items = this.props.data.map(function (element) {
+           return (
+                <div>
+                    {element.title}
+                </div>            
+            );
+        });
+        return (
+            <div className="subcomponent">
+                {items}
+            </div>
+        );
+    }
 });
 
 ReactDOM.render(
-    <Parent />,
+    <Page data={data}/>,
     document.getElementById('app')
 );
